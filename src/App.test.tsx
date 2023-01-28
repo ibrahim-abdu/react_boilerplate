623dd10a6b6b5edf9e2d8a9c3831a5329556f09c
ibromanju <ibromanju@gmail.com>
2023-01-28 02:45:29 -0500

    Test error

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/ሰላም ዓለም/i)
+  const linkElement = screen.getByText(/ሰላም ዓለ/i)
   expect(linkElement).toBeInTheDocument()
 })
